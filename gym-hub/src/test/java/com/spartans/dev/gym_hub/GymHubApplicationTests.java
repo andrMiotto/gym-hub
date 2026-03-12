77cdb3ce7d8817f6452207665536092875f69bfe
andrMiotto <andre_lm_pereira@estudante.sesisenai.org.br>
2026-03-12 16:05:23 -0300

    feat: adiciionar-estrutura-base

---
+++ gym-hub/src/test/java/com/spartans/dev/gym_hub/GymHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartans.dev.gym_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
