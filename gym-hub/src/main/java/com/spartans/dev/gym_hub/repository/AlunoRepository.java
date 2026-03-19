3d390fdf7e1fec19bed8415512308b2f3f1f1f2e
andrMiotto <andre_lm_pereira@estudante.sesisenai.org.br>
2026-03-19 16:57:45 -0300

    feat:implementa-derived-query-methods-Aluno-Professor

--- gym-hub/src/main/java/com/spartans/dev/gym_hub/repository/AlunoRepository.java
+++ gym-hub/src/main/java/com/spartans/dev/gym_hub/repository/AlunoRepository.java
@@ -4,6 +4,9 @@ import com.spartans.dev.gym_hub.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.util.List;
+
 @Repository
-public interface AlunoRepository extends JpaRepository<Aluno,Long> {
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 }
--- gym-hub/src/main/java/com/spartans/dev/gym_hub/repository/ProfessorRepository.java
+++ gym-hub/src/main/java/com/spartans/dev/gym_hub/repository/ProfessorRepository.java
@@ -3,5 +3,12 @@ package com.spartans.dev.gym_hub.repository;
 import com.spartans.dev.gym_hub.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+    Optional<Professor> findByCref(String cref);
+    Boolean existsByCpf(String cpf);
+    Optional<Professor> findByEspecialidade(String especialidade);
+
 }
